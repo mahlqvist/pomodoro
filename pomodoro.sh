713927227390db2037806fcf17466f72282dc4a8
Thomas <mahlqvist@msn.com>
2021-04-28 14:23:52 +0200

    adding while true loop

--- pomodoro.sh
+++ pomodoro.sh
@@ -4,25 +4,32 @@ echo -e "Bash version: ${BASH_VERSION%(*}\n"
 
 img_dir=$(pwd)
 
-echo "Number of minutes to practice: "
-
-read num_min
-
-re='^[0-9]+?$'
-
-while ! [[ $num_min =~ $re ]]
-do
-	echo "Please enter a number: "
+while true
+do		
+	echo "Number of minutes to practice: "
 	read num_min
+	re='^[0-9]+?$'
+
+	while ! [[ $num_min =~ $re ]]
+	do
+		echo "Please enter a number: "
+		read num_min
+	done
+
+	num_sec=$(( $num_min * 1 ))
+
+	sleep $num_sec && notify-send "Well done, time for a break!"
+
+	xdg-open "$img_dir/well_done.jpg"
+	
+	sleep 3 && xdg-open "$img_dir/go_again.jpg"
+
+	echo "Another round [y] or [n]"
+	read ans
+	regex='[Nn]'
+	if [[ $ans =~ $regex ]]
+	then
+		exit 0
+	fi	
 done
-
-num_sec=$(( $num_min * 60 ))
-
-sleep $num_sec && notify-send "Well done, time for a break!"
-
-xdg-open "$img_dir/well_done.jpg"
-
-sleep 300 && notify-send "Time for another round?"
-
-xdg-open "$img_dir/go_again.jpg" && exit 0
 
